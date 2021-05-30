308a46878e45359b3972b00dd54def89bd9ae2e5
srpatel2000 <siddhip2009@gmail.com>
2021-05-30 00:00:27 -0700

    edited feature_prep

---
+++ ec2_initialization.sh
@@ -0,0 +1,7 @@
+export AWS_ACCESS_KEY_ID=ASIAXNMMPAKNHVWP36OB
+export AWS_SECRET_ACCESS_KEY=POVUfqt8l0UUntN9pq2g2PWpViAWHc7ri1xrCPZV
+export AWS_SESSION_TOKEN=FwoGZXIvYXdzEPD//////////wEaDAxRd1HgA5TrWCthYyKsATN0AhvlFZ//mFxxBdvW7V4rXVuow/hW/HIbOmhBkOAKNbOiqdEs2guymAIrlAWppBQ26B/QH6mpv19aKW0yvnjyzzVOjRqK59ee0E1hQpj3C7S8Krc8ftEVOVK5kkVl92bUIgM3R7LBZBbLTsdi2guW37KKA/dj7sOFCy3dolvwMY7HoeMay6IHRGlhoH1t7Hf7q4AObYDPdArCJ7ZIcKAfghD2CdxysKZnqaAope7MhQYyLRaI2mkdyHPlXUGHE70F7YUQcDFItvcNYinCT/4j1ga10ucVSsYDdNnOxzh4kg==
+
+cd dsc_102
+chmod 400 featureprep_ec2.pem
+ssh -i featureprep_ec2.pem ubuntu@ec2-18-237-212-25.us-west-2.compute.amazonaws.com -L 8001:localhost:8787
--- feature_prep.py
+++ feature_prep.py
@@ -6,21 +6,62 @@ from distributed import Client
 client = Client()
 
 # loading in datatable with adjusted datatypes
-df = dd.read_csv("historical_data_2009Q1.txt", sep="|", header=None, dtype={4: int, 25:str, 27: object} )
+og_data_df = dd.read_csv("historical_data_2009Q1.txt", sep="|", header=None, dtype={25:str, 27: object})
+monthly_perf_data_df = dd.read_csv("historical_data_time_2009Q1.txt", sep="|", header=None, dtype={24: 'object', 28: 'object',29: 'object',3: 'object'})
 
 # checking if dataframe was loaded in correctly
-
-# number of rows of original df: 0 621539 
-total_rows = len(df)
+# og_data_df.compute()
+# monthly_perf_data_df.compute()
 
 #### PREPROCESSING ####
+# CLEANING ORIGINATION DATA FILE --------------------------------------------------------------------------------------
 
+# number of rows of original df: 0 621,539 
+# print(len(og_data_df))
+
+new_columns = []
+for col in og_data_df.columns:
+    new_columns.append(str(col))
+og_data_df = og_data_df.rename(columns=dict(zip(og_data_df.columns, new_columns)))
+
+# START : remove or fix null values ----------
 # removing rows with invalid credit scores 
-cleaned_df = df[(df[0] <= 850) & (df[0] >= 301)]
-new_total = len(cleaned_df) # resulting rows: 621494 
+cleaned_df = og_data_df[(og_data_df['0'] <= 850) & (og_data_df['0'] >= 301)]
+# removing rows with invalid debt to income ratio 
+cleaned_df = og_data_df[(og_data_df['9'] <= 65) & (og_data_df['9'] > 0)] # rows: 618,394
 
-# number of null valies in MSA (col 4) -> 126286
-num_null = df.isna().sum(axis=0).compute()
+# number of null values columns
+# print(og_data_df.isna().sum(axis=0).compute())
 
-# drop column: Pre-Harp Loan Sequence Number (#26)
-cleaned_df = cleaned_df.drop(26,axis=1)
+# Pre-Harp Loan Sequence Number (#26) --> has too many nulls & isn't important 
+
+# filled columns with many null values with 'N 
+cleaned_df['25'] = cleaned_df['25'].where(cleaned_df['25'] == 'Y','N')
+cleaned_df['28'] = cleaned_df['28'].where(cleaned_df['28'] == 'Y','N')
+# END : remove or fix null values ------------
+
+# check variance of numerical columns
+# for col in cleaned_df.columns:
+#     if cleaned_df[col].dtype == int or cleaned_df[col].dtype == float:
+#         print(col)
+#         variance = cleaned_df[col].values.var().compute()
+#         print(cleaned_df[col].values.var().compute())
+#         print("\n")
+#         if variance == 0: 
+#             cleaned_df = cleaned_df.drop(col, axis=1) 
+# remove columns with a very low variance
+
+# filter in relevant columns
+cleaned_df = cleaned_df[['0', '4', '7', '8', '9', '11', '12', '14', '17', '19', '25', '28']]
+
+# cleaned_df = cleaned_df[cleaned_df['7'] != 9] 
+
+# CLEANING MONTHLY PERFORMANCE DATA FILE -------------------------------------------------------------------------------
+# print(monthly_perf_data_df.isna().sum(axis=0).compute())
+
+#### FEATURE ENGINEERING ####
+# CLEANING ORIGINATION DATA FILE --------------------------------------------------------------------------------------
+
+# dummy encoding of Metropolitan Statistical Area
+cleaned_df['4'] = cleaned_df['4'].where(cleaned_df['4'] > 0, 0) # indicates that mortgage property is not in MSA
+cleaned_df['4'] = cleaned_df['4'].where(cleaned_df['4'] == 0, 1) # indicates that mortgage property is in MSA
