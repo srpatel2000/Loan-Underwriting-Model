e1c4b97e9840a0dc30388c4c88e77f1f1225f60e
srpatel2000 <siddhip2009@gmail.com>
2021-05-30 16:41:58 -0700

    finalizes EC2 shell instance

--- ec2_initialization.sh
+++ ec2_initialization.sh
@@ -1,17 +1,22 @@
-export AWS_ACCESS_KEY_ID=ASIAXNMMPAKNHVWP36OB
+# HOW TO SSH INTO EC2 INSTANCE ----------------------------------------------------------------------------------------------------
+#cd dsc_102 # [OPTIONAL] # change directories to the folder that has the pem file on siddhi's laptop
+#chmod 400 featureprep_ec2.pem # change permissions in this file to be correct
+#ssh -i featureprep_ec2.pem ubuntu@ec2-18-237-212-25.us-west-2.compute.amazonaws.com -L 8001:localhost:8787 # ssh into EC2 instance
+
+# WHAT TO DO ONCE IN THE EC2 INSTANCE ---------------------------------------------------------------------------------------------
+sudo apt-get update # update your repositories
+sudo apt-get install awscli # upload aws command line interface
+
+# export the API keys onto the instance
+export AWS_ACCESS_KEY_ID=ASIAXNMMPAKNHVWP36OB 
 export AWS_SECRET_ACCESS_KEY=POVUfqt8l0UUntN9pq2g2PWpViAWHc7ri1xrCPZV
 export AWS_SESSION_TOKEN=FwoGZXIvYXdzEPD//////////wEaDAxRd1HgA5TrWCthYyKsATN0AhvlFZ//mFxxBdvW7V4rXVuow/hW/HIbOmhBkOAKNbOiqdEs2guymAIrlAWppBQ26B/QH6mpv19aKW0yvnjyzzVOjRqK59ee0E1hQpj3C7S8Krc8ftEVOVK5kkVl92bUIgM3R7LBZBbLTsdi2guW37KKA/dj7sOFCy3dolvwMY7HoeMay6IHRGlhoH1t7Hf7q4AObYDPdArCJ7ZIcKAfghD2CdxysKZnqaAope7MhQYyLRaI2mkdyHPlXUGHE70F7YUQcDFItvcNYinCT/4j1ga10ucVSsYDdNnOxzh4kg==
 
-cd dsc_102
-chmod 400 featureprep_ec2.pem
-ssh -i featureprep_ec2.pem ubuntu@ec2-18-237-212-25.us-west-2.compute.amazonaws.com -L 8001:localhost:8787
+sudo apt-get install python3-venv # download python virtual env package
+python3 -m venv env # activate venv
+source env/bin/activate # activates this virtual environment as our default python environment
 
-sudo apt-get update
-sudo apt-get install awscli
-sudo apt-get install python3-venv
-python3 -m venv env
-source env/bin/activate
+pip install dask[complete] --no-cache-dir # install dask
+aws s3 cp s3://ds102-mintchoco-scratch/data/historical_data_2009Q1/historical_data_2009Q1.txt ~/ # copy the file you want to read temporarily into your local EC2 instance
+# python # activate python
 
-pip install dask[complete] --no-cache-dir
-aws s3 cp s3://ds102-mintchoco-scratch/data/historical_data_2009Q1/historical_data_2009Q1.txt ~/
-python
