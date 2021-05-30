1b870c74261fb5de98ffa6b39bd69e56ceacf45f
srpatel2000 <siddhip2009@gmail.com>
2021-05-30 16:51:59 -0700

    added EMR instantiation

---
+++ emr_initialization.sh
@@ -0,0 +1,5 @@
+# HOW TO SSH INTO EMR INSTANCE ----------------------------------------------------------------------------------------------------
+# Click on link that says “Connect to the Master Node Using SSH”
+
+# WHAT TO DO ONCE IN THE EMR INSTANCE ---------------------------------------------------------------------------------------------
+pyspark
