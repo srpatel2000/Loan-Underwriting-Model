7299b900610124ee04cbabe06b5b0c5202cf5c21
Nikitha Gopal <nvgopal@ucsd.edu>
2021-05-30 00:39:28 -0700

    updated ec2_sh file

--- ec2_initialization.sh
+++ ec2_initialization.sh
@@ -5,3 +5,13 @@ export AWS_SESSION_TOKEN=FwoGZXIvYXdzEPD//////////wEaDAxRd1HgA5TrWCthYyKsATN0Ahv
 cd dsc_102
 chmod 400 featureprep_ec2.pem
 ssh -i featureprep_ec2.pem ubuntu@ec2-18-237-212-25.us-west-2.compute.amazonaws.com -L 8001:localhost:8787
+
+sudo apt-get update
+sudo apt-get install awscli
+sudo apt-get install python3-venv
+python3 -m venv env
+source env/bin/activate
+
+pip install dask[complete] --no-cache-dir
+aws s3 cp s3://ds102-mintchoco-scratch/data/historical_data_2009Q1/historical_data_2009Q1.txt ~/
+python
