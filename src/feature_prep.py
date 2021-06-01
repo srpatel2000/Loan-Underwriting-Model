95857b5625480525aeed5b57bc52d57527b2ce8e
srpatel2000 <siddhip2009@gmail.com>
2021-05-31 17:40:59 -0700

    fixed comments

--- src/feature_prep.py
+++ src/feature_prep.py
@@ -1,4 +1,4 @@
-# IMPORT STATEMENTS --------------------------------------------------------------------------------------
+# IMPORT STATEMENTS ------------------------------------------------------------------------------------
 import dask.dataframe as dd
 from distributed import Client
 from dask_ml import preprocessing
@@ -6,9 +6,7 @@ import dask.array as da
 import numpy as np
 import sys
 
-
-
-
+### HELPER FUNCTIONS ###
 def replace_null(data, col, val):
     """
     Replaces null values in specific column
@@ -103,7 +101,7 @@ def clean_features(path):
     og_data_df = rename_col(og_data_df) # rename int col to string
 
     #### PREPROCESSING ####
-    # CLEANING ORIGINATION DATA FILE --------------------------------------------------------------------------------------
+    # CLEANING ORIGINATION DATA FILE -------------------------------------------------------------------
     # filter relevant columns
     cleaned_df = og_data_df[['0', '4', '7', '8', '9', '11', '12', '14', '17', '19', '25', '28']]
 
@@ -114,20 +112,20 @@ def clean_features(path):
     # fill null values
     cleaned_df['25'] = replace_null(cleaned_df, '25', 'N') # super conforming flag
     cleaned_df['28'] = replace_null(cleaned_df, '28', 'N') # harp indicator
-    # END OF CLEANING ORIGINATION DATA FILE --------------------------------------------------------------------------------
+    # END OF CLEANING ORIGINATION DATA FILE ------------------------------------------------------------
 
     #### FEATURE ENGINEERING ####
     # Feature #1: using MSA to indicate whether mortage property is in a metropolitan area
     cleaned_df['4'] = cleaned_df['4'].where(cleaned_df['4'] > 0, 0) # not in metropolitan area
     cleaned_df['4'] = cleaned_df['4'].where(cleaned_df['4'] == 0, 1) # in metropolitan area
 
-    # LABEL ENCODING CATEGORICAL VARIABLES ----------------------------------------------------------------------------------
+    # LABEL ENCODING CATEGORICAL VARIABLES -------------------------------------------------------------
     cleaned_df['7'] = encoding(cleaned_df, '7')
     cleaned_df['14'] = encoding(cleaned_df, '14')
     cleaned_df['17'] = encoding(cleaned_df, '17')
     cleaned_df['25'] = encoding(cleaned_df, '25')
     cleaned_df['28'] = encoding(cleaned_df, '28')
-    # END OF LABEL ENCODING CATEGORICAL VARIABLES ----------------------------------------------------------------------------
+    # END OF LABEL ENCODING CATEGORICAL VARIABLES ------------------------------------------------------
 
     return cleaned_df
 
